3b55452daf02f3665fd4c8f92da9f7f212d0e402
Ambresh R C <48542839+ambresh1@users.noreply.github.com>
2022-10-01 22:45:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,8 +77,9 @@ def btTranslator(docxfile):
       files.add_paragraph(translated)
   # translated_text = "\n".join(translated_paragraphs)
   
-  # f=files.save(f"{NewFileName}.docx")
-  return files.save(f"New_File.docx")
+  f=files.save(f"NewFileName.docx")
+  #return files.save(f"New_File.docx")
+  return f
 st.title('Translator App')
 st.markdown("Translate from Docx file")
 st.sidebar.subheader("File Upload")
@@ -86,5 +87,5 @@ st.sidebar.subheader("File Upload")
 datas=st.sidebar.file_uploader("Original File")
 # data=getText("C:\Users\Ambresh C\Desktop\Python Files\Translators\Trail Doc of 500 words.docx")
 
-# st.sidebar.download_button(label='Download Translated File',file_name='Translated.docx')
-st.write(btTranslator(datas))
+st.sidebar.download_button(label='Download Translated File',file_name='Translated.docx', data=btTranslator(datas)) 
+#st.write(btTranslator(datas))
