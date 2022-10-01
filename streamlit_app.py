36a7bdbcd2609e066b672a970b131253f1a7ead0
Ambresh R C <48542839+ambresh1@users.noreply.github.com>
2022-10-01 17:43:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,4 +87,4 @@ datas=st.sidebar.file_uploader("Original File")
 # data=getText("C:\Users\Ambresh C\Desktop\Python Files\Translators\Trail Doc of 500 words.docx")
 
 # st.sidebar.download_button(label='Download Translated File',file_name='Translated.docx')
-st.write(getText(datas))
+st.write(btTranslator(datas))
