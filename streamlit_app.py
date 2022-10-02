fac4f6057ffd1649a17e05c3654cce178b8e06a5
Ambresh R C <48542839+ambresh1@users.noreply.github.com>
2022-10-02 12:32:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,7 +83,7 @@ def btTranslator(docxfile):
   #files=files.save("Translated.docx")
   binary_output = BytesIO()
   f=files.save(binary_output)
-  return f
+  return f.getvalue()
   #return translated_text
 st.title('Translator App')
 st.markdown("Translate from Docx file")
@@ -92,5 +92,5 @@ st.sidebar.subheader("File Upload")
 datas=st.sidebar.file_uploader("Original File")
 #data=getText("C:\Users\Ambresh C\Desktop\Python Files\Translators\Trail Doc of 500 words.docx")
 
-st.sidebar.download_button(label='Download Translated File',file_name='Translated.docx', data=btTranslator(datas).getvalue(),mime="text/csv") 
+st.sidebar.download_button(label='Download Translated File',file_name='Translated.docx', data=btTranslator(datas),mime="text/csv") 
 # st.text_area(label="",value=btTranslator(datas),height=100)
